12b64cd49610122283c2fa8cdc23ef3d6c29b1d8
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-19 14:51:46 +0200

    update version to 9

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Taks to calendar events",
-    'version': '1.0',
+    'version': '9.0.1.0',
     'depends': ['project', 'calendar'],
     'author': "Valentin THIRION, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
